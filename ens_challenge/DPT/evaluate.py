8889db4da484d6daaf02e1d872421b39279243c6
YunfeiZHAO <moizhaoyunfei@gmail.com>
2021-11-18 18:34:10 +0100

    dpt

---
+++ ens_challenge/DPT/evaluate.py
@@ -0,0 +1,78 @@
+import torch
+from torch.utils.data import DataLoader
+
+from tqdm import tqdm
+from utils.utils import calculate_kl_loss, write_label_csv
+
+from datasets.satellite import SatelliteDataset
+
+
+@torch.no_grad()
+def evaluate(net, dataloader, device):
+    """
+    Calculate dice score, cross_entropy, KL loss
+    """
+    net.eval()
+    num_val_batches = len(dataloader)
+    val_crossentropy_loss_mask = 0
+    val_crossentropy_loss_ratio = 0
+    mask_crossentropy = torch.nn.CrossEntropyLoss().to(device)
+    ratio_crossentropy = torch.nn.CrossEntropyLoss()
+    all_image_ids = torch.tensor([])
+    true_labels = torch.tensor([])
+    predict_labels = torch.tensor([])
+
+    # iterate over the validation set
+    for batch in tqdm(dataloader, total=num_val_batches, desc='Validation round', unit='batch', leave=False):
+        image_ids, image, mask_true, ratio_label = batch['image_id'], batch['image'], batch['mask'], batch['label']
+        # move images and labels to correct device and type
+        image = image.to(device=device, dtype=torch.float32)
+        mask_true = mask_true.to(device=device, dtype=torch.long)
+
+        # image ids
+        all_image_ids = torch.cat((all_image_ids, image_ids))
+
+        # true label
+        true_labels = torch.cat((true_labels, ratio_label))
+
+        # predict the mask
+        mask_pred = net(image)  # B, C, h, w
+
+        # predicted class ratio by sum of each channel
+        batch_ratio = torch.sum(mask_pred, (-2, -1))  # B, C
+        batch_ratio /= torch.sum(batch_ratio, 1)[..., None]
+        batch_ratio = batch_ratio.cpu()
+        predict_labels = torch.cat((predict_labels, batch_ratio))
+
+        # crossentropy_loss on mask
+        val_crossentropy_loss_mask += mask_crossentropy(mask_pred, mask_true).float().cpu()
+        # crossentropy_loss on ratio
+        val_crossentropy_loss_ratio += ratio_crossentropy(batch_ratio, ratio_label)
+
+
+    write_label_csv(all_image_ids, predict_labels, '/home/yunfei/Desktop/m2a_sorbonne/ens_challenge/experiments/evaluation_predict_labels.csv')
+    write_label_csv(all_image_ids, true_labels, '/home/yunfei/Desktop/m2a_sorbonne/ens_challenge/experiments/evaluation_true_labels.csv')
+
+    # KL_score
+    # take into account only last 8 classes
+    kl_loss = calculate_kl_loss(predict_labels[:, 2:10], true_labels[:, 2:10])
+    net.train()
+
+    return val_crossentropy_loss_mask, val_crossentropy_loss_ratio, kl_loss
+
+
+# if __name__ == '__main__':
+#     # tensorboard_dir = '../val'
+#     # writer = SummaryWriter(log_dir=tensorboard_dir)
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     val_set = SatelliteDataset(index_txt_path='/home/yunfei/Desktop/m2a_sorbonne/ens_challenge/val_index.csv',
+#                                images_folder_path='/home/yunfei/Desktop/m2a_sorbonne/ens_challenge/dataset/',
+#                                type='val')
+#     loader_args = dict(batch_size=8, num_workers=4, pin_memory=True)
+#     val_loader = DataLoader(val_set, shuffle=False, drop_last=False, **loader_args)
+#     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#     net = UNet(n_channels=4, n_classes=10, bilinear=True).to(device)
+#     load = '/home/yunfei/Desktop/m2a_sorbonne/ens_challenge/checkpoints/test/checkpoint_epoch6.pth'
+#     net.load_state_dict(torch.load(load, map_location=device))
+#     val_crossentropy_loss_mask, val_crossentropy_loss_ratio, kl_loss = evaluate(net, val_loader, device)
+#     print(f"val_crossentropy_loss_mask: {val_crossentropy_loss_mask}, val_crossentropy_loss_ratio: {val_crossentropy_loss_ratio}, kl_loss: {kl_loss}")
---
+++ ens_challenge/DPT/utils/utils.py
@@ -0,0 +1,151 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import torch
+
+
+class LandCoverData():
+    """Class to represent the S2GLC Land Cover Dataset for the challenge,
+    with useful metadata and statistics.
+    """
+    # image size of the images and label masks
+    IMG_SIZE = 256
+    # the images are RGB+NIR (4 channels)
+    N_CHANNELS = 4
+    # we have 9 classes + a 'no_data' class for pixels with no labels (absent in the dataset)
+    N_CLASSES = 10
+    CLASSES = [
+        'no_data',
+        'clouds',
+        'artificial',
+        'cultivated',
+        'broadleaf',
+        'coniferous',
+        'herbaceous',
+        'natural',
+        'snow',
+        'water'
+    ]
+    # classes to ignore because they are not relevant. "no_data" refers to pixels without
+    # a proper class, but it is absent in the dataset; "clouds" class is not relevant, it
+    # is not a proper land cover type and images and masks do not exactly match in time.
+    IGNORED_CLASSES_IDX = [0, 1]
+
+    # The training dataset contains 18491 images and masks
+    # The test dataset contains 5043 images and masks
+    TRAINSET_SIZE = 18491
+    TESTSET_SIZE = 5043
+
+    # for visualization of the masks: classes indices and RGB colors
+    CLASSES_COLORPALETTE = {
+        0: [0,0,0],
+        1: [255,25,236],
+        2: [215,25,28],
+        3: [211,154,92],
+        4: [33,115,55],
+        5: [21,75,35],
+        6: [118,209,93],
+        7: [130,130,130],
+        8: [255,255,255],
+        9: [43,61,255]
+        }
+    CLASSES_COLORPALETTE = {c: torch.tensor(color, dtype=torch.float) for (c, color) in CLASSES_COLORPALETTE.items()}
+
+    # statistics
+    # the pixel class counts in the training set
+    TRAIN_CLASS_COUNTS = np.array(
+        [0, 20643, 60971025, 404760981, 277012377, 96473046, 333407133, 9775295, 1071, 29404605]
+    )
+    # the minimum and maximum value of image pixels in the training set
+    TRAIN_PIXELS_MIN = 1
+    TRAIN_PIXELS_MAX = 24356
+
+
+def plot_img_and_mask(img, mask):
+    classes = mask.shape[0] if len(mask.shape) > 2 else 1
+    fig, ax = plt.subplots(1, classes + 1)
+    ax[0].set_title('Input image')
+    ax[0].imshow(img)
+    if classes > 1:
+        for i in range(classes):
+            ax[i + 1].set_title(f'Output mask (class {i + 1})')
+            ax[i + 1].imshow(mask[:, :, i])
+    else:
+        ax[1].set_title(f'Output mask')
+        ax[1].imshow(mask)
+    plt.xticks([]), plt.yticks([])
+    plt.show()
+
+
+def show_mask(mask_tensor):
+    # masks_tensor: H, W
+    show_mask = torch.zeros((*mask_tensor.size(), 3))
+    for c, color in LandCoverData.CLASSES_COLORPALETTE.items():
+        show_mask[mask_tensor == c, :] = color / 255
+    return show_mask
+
+
+def show_image(image_tensor):
+    """ Show a image tensor loaded from dataloader
+    :param image_tensor: C,H,W
+    return a displayable tensor
+    """
+    # for image diaplay in tensorboard
+    display_min = 50
+    display_max = 3000
+    image = image_tensor * LandCoverData.TRAIN_PIXELS_MAX
+    image = image.clip(display_min, display_max)
+    image = image / 1000  # divide by empirical mean
+    image = torch.as_tensor(image.clip(0, 1), dtype=torch.float)
+    return image
+
+
+def ratio(mask):
+    batch_size = mask.shape[0]
+    size = mask.shape[1]
+    res = torch.zeros((batch_size, 10), dtype=torch.float64)
+    for i in np.arange(2, 10):
+        res[:, i] = (mask == i).sum(dim=(1, 2))/(size*size)
+    return res
+
+
+def write_csv(index, matrix, path):
+    #index must be a tensor with size = batch_size
+    #matrix must be a tensor withe shape =batch_size * 256 * 256
+    batch_size = matrix.shape[0]
+    idx = index.reshape(batch_size, -1)
+    myratio = ratio(matrix)
+    data_pure = torch.cat((idx, myratio), dim=1)
+    data = pd.DataFrame(data_pure.numpy() , columns = ['sample_id','no_data','clouds','artificial','cultivated','broadleaf','coniferous','herbaceous','natural','snow','water'])
+    # post processing
+    data = data.astype({'sample_id': 'int'})
+    # change nature and snow to 0
+    data["natural"] = data["natural"] * 0
+    data["snow"] = data["snow"] * 0
+    data.to_csv(path, index=False)
+    print("make cvs done, with batch size = ", batch_size)
+
+
+def calculate_kl_loss(true_labels, predict_labels):
+    """calculate kl loss from two tensors if B, C"""
+    predict_labels /= predict_labels.sum(dim=1)[..., None]
+    true_labels /= true_labels.sum(dim=1)[..., None]
+    kl_loss = torch.mean(torch.sum(true_labels * np.log((true_labels + 1e-7) / (predict_labels + 1e-7)), 1))
+    try:
+        assert torch.isfinite(kl_loss)
+    except AssertionError as e:
+        raise ValueError('kl loss is NaN or infinite') from e
+    return kl_loss
+
+
+def write_label_csv(index, matrix, path):
+    """Generate true label from dataloader"""
+    #index must be a tensor with size = batch_size
+    #matrix must be a tensor withe shape =batch_size * 10
+    batch_size = matrix.shape[0]
+    idx = index.reshape(batch_size, -1)
+    data_pure = torch.cat((idx, matrix), dim=1)
+    data = pd.DataFrame(data_pure.numpy(), columns=['sample_id','no_data','clouds','artificial','cultivated','broadleaf','coniferous','herbaceous','natural','snow','water'])
+    data = data.astype({'sample_id': 'int'})
+    data.to_csv(path, index=False)
+    print("make cvs true label done, with batch size = ", batch_size)
