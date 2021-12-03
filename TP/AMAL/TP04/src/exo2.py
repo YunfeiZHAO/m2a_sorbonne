6f5290a00a1283fa0f005a8880fbe1ad9451a1b1
YunfeiZHAO <moizhaoyunfei@gmail.com>
2021-12-03 20:18:55 +0100

    ok

--- TP/AMAL/TP04/src/exo2.py
+++ TP/AMAL/TP04/src/exo2.py
@@ -1,5 +1,7 @@
+from tqdm import tqdm
+
 from torch.nn.modules import loss
-from utils import RNN, device, SampleMetroDataset
+from utils import RNN, device, SampleMetroDataset, AverageMeter
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
@@ -14,40 +16,67 @@ DIM_INPUT = 2
 #Taille du batch
 BATCH_SIZE = 32
 
-PATH = "../../data/"
+PATH = "../data/"
 
-matrix_train, matrix_test = torch.load(open(PATH + "hzdataset.pch","rb"))
+# Tensorboard : rappel, lancer dans une console tensorboard --logdir runs
+writer = SummaryWriter("../experiments/hangzhou_rnn" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+matrix_train, matrix_test = torch.load(open(PATH + "hzdataset.pch", "rb"))
 ds_train = SampleMetroDataset(matrix_train[:, :, :CLASSES, :DIM_INPUT], length=LENGTH)
-ds_test = SampleMetroDataset(matrix_test[:, :, :CLASSES, :DIM_INPUT], length = LENGTH, stations_max = ds_train.stations_max)
+ds_test = SampleMetroDataset(matrix_test[:, :, :CLASSES, :DIM_INPUT], length=LENGTH, stations_max=ds_train.stations_max)
 data_train = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True)
 data_test = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=False)
 
 
 #  TODO:  Question 2 : prédiction de la ville correspondant à une séquence
-
-
 def train(epoch, data_train, model, loss_fun, optimizer, writer):
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
     iter = epoch*len(data_train)
+    model.train()
     for batch, target in data_train:
-        batch = torch.transpose(batch, 0, 1)
+        batch = torch.transpose(batch, 0, 1)  # swap the first and second dimension, make it T, B, d
         prediction = model(batch)
-        prediction = model.decode(prediction[-1, :, :])
+        prediction = model.decode(prediction[-1, :, :])  # the prediction come from the last hidden layer output
         loss = loss_fun(prediction, target)
         acc = accuracy(prediction, target)
-        print("Epoch: {}; Iteration: {}; Loss: {}, Accuracy: {}".format(epoch, iter, loss, acc))
+        loss_meter.update(loss)
+        acc_meter.update(acc)
+        # print("Epoch: {}; Iteration: {}; Loss: {}, Accuracy: {}".format(epoch, iter, loss, acc))
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         iter += 1
-        writer.add_scalar("Loss", loss, iter)
-        writer.add_scalar("Accuracy", acc, iter)
+        writer.add_scalar("Train/Loss", loss, iter)
+        writer.add_scalar("Train/Accuracy", acc, iter)
+    writer.add_scalar("Train/Epoch Loss", loss_meter.avg, epoch)
+    writer.add_scalar("Train/Epoch Accuracy", acc_meter.avg, epoch)
+
+
+def evaluate(epoch, data_test, model, loss_fun, writer):
+    model.eval()
+    loss_meter = AverageMeter()
+    acc_meter = AverageMeter()
+    iter = epoch*len(data_test)
+    for batch, target in data_test:
+        batch = torch.transpose(batch, 0, 1)  # swap the first and second dimension, make it T, B, d
+        prediction = model(batch)
+        prediction = model.decode(prediction[-1, :, :])  # the prediction come from the last hidden layer output
+        loss = loss_fun(prediction, target)
+        acc = accuracy(prediction, target)
+        loss_meter.update(loss)
+        acc_meter.update(acc)
+        iter += 1
+        # print("Epoch: {}; Iteration: {}; Loss: {}, Accuracy: {}".format(epoch, iter, loss, acc))
+        writer.add_scalar("Test/Loss", loss, iter)
+        writer.add_scalar("Test/Accuracy", acc, iter)
+    writer.add_scalar("Test/Epoch Loss", loss_meter.avg, epoch)
+    writer.add_scalar("Test/Epoch Accuracy", acc_meter.avg, epoch)
+
 
 def accuracy(prediction, target):
     prediction = torch.argmax(prediction, dim=1)
-    return torch.mean((prediction==target).float())
-
-# Tensorboard : rappel, lancer dans une console tensorboard --logdir runs
-writer = SummaryWriter("runs/runs"+datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+    return torch.mean((prediction == target).float())
 
 
 model = RNN(DIM_INPUT, 100, CLASSES)
@@ -56,9 +85,12 @@ loss_fun = torch.nn.CrossEntropyLoss()
 
 epochs = 1000
 
-for epoch in range(epochs):
+for epoch in tqdm(range(epochs)):
     train(epoch, data_train, model, loss_fun, optimizer, writer)
-"""
+    evaluate(epoch, data_test, model, loss_fun, writer)
+
+
+""" data analyse
 i = 0
 for x, y in data_train:
     x = torch.transpose(x, 0, 1)
---
+++ TP/rdfia/section3/function.py
@@ -0,0 +1,5 @@
+activation = {}
+def get_activation(name):
+    def hook(model, input, output):
+        activation[name] = output.detach()
+    return hook
