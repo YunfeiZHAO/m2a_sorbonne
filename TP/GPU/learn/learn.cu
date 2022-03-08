f0a02c21796d22c5c64d22816f9c34f5b48fe5ac
YunfeiZHAO <moizhaoyunfei@gmail.com>
2022-03-08 12:06:54 +0100

    ok

--- TP/GPU/learn/learn.cu
+++ TP/GPU/learn/learn.cu
@@ -42,10 +42,12 @@ int main(void) {
     int size = (N + 2*RADIUS) * sizeof(int);
     // Alloc space for host copies and setup values
     in = (int *)malloc(size); fill_ints(in, N + 2*RADIUS);
-    out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);	serial code
+    out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);
+    
     // Alloc space for device copies
     cudaMalloc((void **)&d_in, size);
 	cudaMalloc((void **)&d_out, size);
+
     // Copy to device
     cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice);
     cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
@@ -56,8 +58,8 @@ int main(void) {
 
     // Copy result back to host
     cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
+
     // Cleanup
-    serial code
     free(in); free(out);
     cudaFree(d_in); cudaFree(d_out);
     return 0;
